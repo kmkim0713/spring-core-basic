582893e840eb04eff861a1769f613a68235d59d3
kmkim0713 <kmkimworkwork@gmail.com>
2024-09-25 17:00:00 +0900

    feat: first commit

---
+++ src/main/java/com/example/springclass/SpringClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.springclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClassApplication.class, args);
+	}
+
+}
