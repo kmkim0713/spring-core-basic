2faf2f82986639e8bcad5850c2c6fc8331a99e74
kmkim0713 <kmkimworkwork@gmail.com>
2024-09-26 17:29:29 +0900

    feat: Spring 전환 시작. AppConfig 리팩토링

--- src/main/java/com/example/springclass/AppConfig.java
+++ src/main/java/com/example/springclass/AppConfig.java
@@ -2,27 +2,33 @@ package com.example.springclass;
 
 import com.example.springclass.discount.DiscountPolicy;
 import com.example.springclass.discount.FixDiscountPolicy;
-import com.example.springclass.discount.RateDiscountPolicy;
 import com.example.springclass.member.MemberService;
 import com.example.springclass.member.MemberServiceImpl;
 import com.example.springclass.member.MemoryMemberRepository;
 import com.example.springclass.order.OrderService;
 import com.example.springclass.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
 //        return new RateDiscountPolicy();
