66278739ea4156fb725917ab9a2542690c47cb84
kmkim0713 <kmkimworkwork@gmail.com>
2024-09-26 17:37:47 +0900

    feat: OrderApp Spring 전환

--- src/main/java/com/example/springclass/OrderApp.java
+++ src/main/java/com/example/springclass/OrderApp.java
@@ -4,14 +4,15 @@ import com.example.springclass.member.*;
 import com.example.springclass.order.Order;
 import com.example.springclass.order.OrderService;
 import com.example.springclass.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-
-
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
