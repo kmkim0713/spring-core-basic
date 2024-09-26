46908c1347291176bb712dc07a7be9ae59acd2d2
kmkim0713 <kmkimworkwork@gmail.com>
2024-09-26 17:35:18 +0900

    feat: MemberApp Spring 전환

--- src/main/java/com/example/springclass/MemberApp.java
+++ src/main/java/com/example/springclass/MemberApp.java
@@ -4,13 +4,18 @@ import com.example.springclass.member.Grade;
 import com.example.springclass.member.Member;
 import com.example.springclass.member.MemberService;
 import com.example.springclass.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl(memberRepository);
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // AppConfig의 메서드이름과 맵핑
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
