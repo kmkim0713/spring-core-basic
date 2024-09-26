43edc6c682d8a9d16e3fce71a4ea9164713c8722
kmkim0713 <kmkimworkwork@gmail.com>
2024-09-26 17:20:18 +0900

    feat: AppConfig 리팩토링

--- src/main/java/com/example/springclass/AppConfig.java
+++ src/main/java/com/example/springclass/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.springclass;
 
+import com.example.springclass.discount.DiscountPolicy;
 import com.example.springclass.discount.FixDiscountPolicy;
+import com.example.springclass.discount.RateDiscountPolicy;
 import com.example.springclass.member.MemberService;
 import com.example.springclass.member.MemberServiceImpl;
 import com.example.springclass.member.MemoryMemberRepository;
@@ -10,12 +12,20 @@ import com.example.springclass.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
+    }
 
 }
