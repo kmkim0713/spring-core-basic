451809e316b63cd792ca19eea4217f6ba3cfc331
kmkim0713 <kmkimworkwork@gmail.com>
2024-09-27 09:40:16 +0900

    feat: ApplicationContextInfoTest() 테스트 추가

---
+++ src/test/java/com/example/springclass/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,37 @@
+package com.example.springclass.beanfind;
+
+import com.example.springclass.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+    @Test
+    @DisplayName("모든 빈 출력")
+    void findAllBeans(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames(); // iter 단축키로 for문 자동 생성
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력")
+    void findApplicationBeans(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames(); // iter 단축키로 for문 자동 생성
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == beanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+}
